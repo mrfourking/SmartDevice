9ce8741717a5573e9a346caf18691485a6171184
Ilya Fokin <ilyafokinn@yandex.ru>
2020-03-03 14:54:49 +0300

    Добавляет модальное окно с формой обратной связи

--- build/js/main.js
+++ build/js/main.js
@@ -52,10 +52,100 @@
 
 (function () {
   $(function () {
-    $('#phone').inputmask('+7(9{3})9{3}-9{2}-9{2}', {jitMasking: 3});
+    $('#phone').inputmask('+7(9{3})9{3}-9{2}-9{2}', {
+      jitMasking: 3
+    });
   });
 
   $(function () {
-    $('#phone-modal').inputmask('+7(9{3})9{3}-9{2}-9{2}', {jitMasking: 3});
+    $('#phone-modal').inputmask('+7(9{3})9{3}-9{2}-9{2}', {
+      jitMasking: 3
+    });
+  });
+})();
+
+(function () {
+  var openButton = document.querySelector('.header__callback-button');
+  var modal = document.querySelector('.modal');
+  var closeButton = modal.querySelector('.callback-form__button-close');
+  var userName = modal.querySelector('#name-modal');
+  var userPhone = modal.querySelector('#phone-modal');
+  var userMessage = modal.querySelector('#question-modal');
+
+  var isStorageSupport = true;
+  var storageName = '';
+  var storagePhone = '';
+  var storageMessage = '';
+
+  try {
+    storageName = localStorage.getItem('userName');
+    storagePhone = localStorage.getItem('userPhone');
+    storageMessage = localStorage.getItem('userMessage');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  var closeModal = function () {
+    modal.classList.remove('modal--open');
+
+    document.removeEventListener('keydown', onModalKeydown);
+    document.removeEventListener('click', onRandomAreaClick);
+  };
+
+  var onModalButtonsClick = function () {
+    closeModal();
+
+    if (isStorageSupport) {
+      localStorage.setItem('userName', userName.value);
+      localStorage.setItem('userPhone', userPhone.value);
+      localStorage.setItem('userMessage', userMessage.value);
+    }
+  };
+
+  var onModalKeydown = function (evt) {
+    if (evt.keyCode === 27) {
+      closeModal();
+    }
+  };
+
+  // var on = function (evt) {
+  //   var innerSuccessBlock = window.render.mainBlock.querySelector('.success__inner');
+  //   if (evt.target !== innerSuccessBlock && !(innerSuccessBlock.contains(evt.target))) {
+  //     closeSuccessBlock();
+  //   }
+  // };
+
+  var onRandomAreaClick = function (evt) {
+    if (evt.target !== modal && !(modal.contains(evt.target))) {
+      closeModal();
+    }
+  };
+
+
+  openButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    modal.classList.add('modal--open');
+    userName.focus();
+
+    if (isStorageSupport) {
+      userName.value = storageName;
+      userPhone.value = storagePhone;
+      userMessage.value = storageMessage;
+    }
+
+    document.addEventListener('keydown', onModalKeydown);
+    document.addEventListener('click', onRandomAreaClick);
+  });
+
+  modal.addEventListener('submit', function () {
+    onModalButtonsClick();
+  });
+
+  closeButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    onModalButtonsClick();
   });
 })();
